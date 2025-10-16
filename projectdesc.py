554f74b6cd0568b7fbef63e8073e80a740f86e3a
MV2 <maheshchary660@gmail.com>
2025-10-16 16:37:53 +0530

    Update projectdesc.py

--- projectdesc.py
+++ projectdesc.py
@@ -3,6 +3,8 @@
 # client main code - lalitha
 
 
+SERVER SIDE CODE
+--------------------------
 # def updategamedata(name,turns):
 #   f=open("gamedata.txt","a")
 #   f.write(f"{name},{turns}\n")
@@ -27,16 +29,42 @@
 #  - update_orders(order_id,status) - return True/False
 #  - read_orders() - returns list of orders
 
+
+
+CLIENT SIDE CODE
+-------------------------
+class StockItem
+  product_id
+  name
+  price
+  available_quantity
+
+class Stock
+- products = [list of stockitem objects]
+- update_stock(stockstring) 
+  - empty the products list
+  - process the stockstring and create objects for StockItem and append to products list
+- display_stock() - print the available products to the customer(below)
+"""
+MyKart Store
+1. Laptop-Rs.34000 2.Keyboard-Rs.800 3.Mouse-Rs.300 4.Bluetooth Speaker-Rs.1200
+"""
+
+
+
 # class OnboardingUser - manu
 #  - start_registration()
 #    - collect username, password, address, phone
-#    - add data to file using Database class
-#    - create a debit card with 1L balance, card no should be unique, create it using the last debit card no in file
+#    - server ki data send cheyyali client socket use chesi send() function vadi
+     - server daggara nunchi success/fail message collect chesko recv() function tho
+#      - success aithe -> registration successful ani print chesi, server nunchi vachina debit card details print cheyyi
+       - fail aithe -> registration failed ani cheppi reason print cheyyi. reason server send chesina response lo untadhi(e.g: user already exists)
 #    - return True/False
 
 #  - start_login()
 #    - collect username and password
-#    - verify username and password
+#    - server ki data send cheyyali client socket use chesi send() function vadi
+     - server daggara nunchi success/fail message collect chesko recv() function tho
 #    - if valid, return True
 #    - if invalid, return False
 
